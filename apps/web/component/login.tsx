4c9e3c1e6ca55d16cb4a3c4b843968e4ccb5281a
ANSHSINGH050404 <114014895+ANSHSINGH050404@users.noreply.github.com>
2026-05-22 17:11:54 +0530

    first commit

---
+++ apps/web/component/login.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Login = () => {
+  return <div  className="flex flex-col" >
+    <input type="text"  className=""  placeholder="Enter Your Email"/>
+    <input type="text"  className=""  placeholder="Enter Your PassWord"/>
+  </div>;
+};
